26a641c8806271a2ed280e0f54d574dbf58a4b4f
oshou <adf1985adf@gmail.com>
2019-08-30 06:39:26 +0900

    [WIP]Controller修正

--- server/server.go
+++ server/server.go
@@ -21,22 +21,28 @@ func router() *gin.Engine {
 
 		// ユーザー一覧
 		u.GET("/users", ctrl.GetUsers)
-		// ユーザー表示
-		u.GET("/posts/:post_id", ctrl.GetPosts)
 		// ユーザー追加
-		u.PUT("/posts/:post_id", ctrl.AddPosts)
+		u.POST("/users", ctrl.AddUser)
+		// 投稿一覧
+		u.GET("/posts", ctrl.GetPosts)
+		// 投稿追加
+		u.POST("/posts", ctrl.AddPost)
+		// 投稿表示
+		u.GET("/posts/:post_id", ctrl.GetPost)
+		// 投稿削除
+		u.DELETE("/posts/:post_id", ctrl.DeletePost)
 		// コメント一覧
 		u.GET("/posts/:post_id/comments", ctrl.GetComments)
 		// コメント追加
-		u.PUT("/posts/:post_id/comments", ctrl.AddComment)
+		u.POST("/posts/:post_id/comments", ctrl.AddComment)
 		// コメント表示
-		u.GET("/posts/:post_id/comments/:comment_id", ctrl.getComment)
+		u.GET("/posts/:post_id/comments/:comment_id", ctrl.GetComment)
 		// コメント削除
-		u.DELETE("/posts/:post_id/comments/:comment_id", ctrl.getComment)
+		u.DELETE("/posts/:post_id/comments/:comment_id", ctrl.DeleteComment)
 		// タグ一覧
-		u.GET("/tags", ctrl.ShowScraping)
+		u.GET("/tags", ctrl.GetTags)
 		// 検索結果
-		u.GET("/search", ctrl.ShowMail)
+		u.GET("/search", ctrl.SearchByType)
 	}
 
 	return r
