1a650be3536abafc4bfd45df537b45c595e8f1f7
oshou <adf1985adf@gmail.com>
2019-08-31 07:21:07 +0900

    [WIP] packageパス修正

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -2,7 +2,7 @@ package controller
 
 import(
 	"fmt"
-	"github.com/oshou/AwesomeMusic-api/service"
+	"github.com/oshou/AwesomeMusic-api/user"
 	"github.com/gin-gonic/gin"
 )
 
@@ -38,129 +38,129 @@ func (c Controller) AddUser(ctx *gin.Context) {
 	}
 }
 
-// Index action: GET /v1/posts
-func (c Controller) GetPosts(ctx *gin.Context) {
-
-	var s post.Service
-	val, err := s.GetPosts()
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-// Index action: POST /v1/posts
-func (c Controller) AddPost(ctx *gin.Context) {
-	var s post.Service
-	val, err := s.AddPost()
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-// Index action: GET /v1/posts/:post_id
-func (c Controller) GetPost(ctx *gin.Context) {
-
-	post_id := ctx.Param("post_id")
-
-	var s post.Service
-	val, err := s.GetPost(post_id)
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-// Index action: DELETE /v1/posts/:post_id
-func (c Controller) DeletePost(ctx *gin.Context) {
-
-	post_id := ctx.Param("post_id")
-
-	var s post.Service
-	val, err := s.DeletePost(post_id)
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-// Index action: GET /v1/posts/:post_id/comments
-func (c Controller) GetComments(ctx *gin.Context) {
-
-	post_id := ctx.Param("post_id")
-
-	var s post.Service
-	val, err := s.GetComments(post_id)
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-
-// Index action: POST /v1/posts/:post_id/comments
-func (c Controller) AddComment(ctx *gin.Context) {
-
-	post_id := ctx.Param("post_id")
-	user_id := ctx.Query("user_id")
-
-	var s post.Service
-	val, err := s.AddComment(post_id,comment_id)
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-// Index action: GET /v1/posts/:post_id/comments/:comment_id
-func (c Controller) GetComment(ctx *gin.Context) {
-
-	post_id := ctx.Param("post_id")
-	comment_id := ctx.Param("comment_id")
-
-	var s post.Service
-	val, err := s.GetComment(post_id,comment_id)
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
-
-// Index action: DELETE /v1/posts/:post_id/comments/:comment_id
-func (c Controller) DeleteComment(ctx *gin.Context) {
-
-	post_id := ctx.Param("post_id")
-	comment_id := ctx.Param("comment_id")
-
-	var s post.Service
-	val, err := s.DeleteComment(comment_id)
-
-	if err != nil {
-		ctx.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		ctx.JSON(200, val)
-	}
-}
+// // Index action: GET /v1/posts
+// func (c Controller) GetPosts(ctx *gin.Context) {
+// 
+// 	var s post.Service
+// 	val, err := s.GetPosts()
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// // Index action: POST /v1/posts
+// func (c Controller) AddPost(ctx *gin.Context) {
+// 	var s post.Service
+// 	val, err := s.AddPost()
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// // Index action: GET /v1/posts/:post_id
+// func (c Controller) GetPost(ctx *gin.Context) {
+// 
+// 	post_id := ctx.Param("post_id")
+// 
+// 	var s post.Service
+// 	val, err := s.GetPost(post_id)
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// // Index action: DELETE /v1/posts/:post_id
+// func (c Controller) DeletePost(ctx *gin.Context) {
+// 
+// 	post_id := ctx.Param("post_id")
+// 
+// 	var s post.Service
+// 	val, err := s.DeletePost(post_id)
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// // Index action: GET /v1/posts/:post_id/comments
+// func (c Controller) GetComments(ctx *gin.Context) {
+// 
+// 	post_id := ctx.Param("post_id")
+// 
+// 	var s post.Service
+// 	val, err := s.GetComments(post_id)
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// 
+// // Index action: POST /v1/posts/:post_id/comments
+// func (c Controller) AddComment(ctx *gin.Context) {
+// 
+// 	post_id := ctx.Param("post_id")
+// 	user_id := ctx.Query("user_id")
+// 
+// 	var s post.Service
+// 	val, err := s.AddComment(post_id,comment_id)
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// // Index action: GET /v1/posts/:post_id/comments/:comment_id
+// func (c Controller) GetComment(ctx *gin.Context) {
+// 
+// 	post_id := ctx.Param("post_id")
+// 	comment_id := ctx.Param("comment_id")
+// 
+// 	var s post.Service
+// 	val, err := s.GetComment(post_id,comment_id)
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
+// 
+// // Index action: DELETE /v1/posts/:post_id/comments/:comment_id
+// func (c Controller) DeleteComment(ctx *gin.Context) {
+// 
+// 	post_id := ctx.Param("post_id")
+// 	comment_id := ctx.Param("comment_id")
+// 
+// 	var s post.Service
+// 	val, err := s.DeleteComment(comment_id)
+// 
+// 	if err != nil {
+// 		ctx.AbortWithStatus(404)
+// 		fmt.Println(err)
+// 	} else {
+// 		ctx.JSON(200, val)
+// 	}
+// }
