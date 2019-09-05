f61d45944ee8069f2de44557b9d4f81ad6f64451
oshou <adf1985adf@gmail.com>
2019-09-05 23:09:33 +0900

    postテーブルにタイトルカラム追加

--- service/search_service.go
+++ service/search_service.go
@@ -9,13 +9,27 @@ type SearchService struct{}
 
 //type Post entity.Post
 
+func (ss SearchService) GetByPostTitle(q string) ([]Post, error) {
+
+	var p []Post
+
+	stmt := db.GetDBConn()
+	stmt = stmt.Table("post")
+	stmt = stmt.Select("id,user_id,title,url,message")
+	stmt = stmt.Where("post.title LIKE ?", "%"+q+"%")
+	if err := stmt.Find(&p).Error; err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
 func (ss SearchService) GetByUserId(q string) ([]Post, error) {
 
 	var p []Post
 
 	stmt := db.GetDBConn()
 	stmt = stmt.Table("post")
-	stmt = stmt.Select("post.id,post.url,post.message")
+	stmt = stmt.Select("post.id,post.user_id,post.title,post.url,post.message")
 	stmt = stmt.Joins("INNER JOIN user on user.id = post.user_id")
 	stmt = stmt.Where("user.id = ?", q)
 	if err := stmt.Find(&p).Error; err != nil {
@@ -30,7 +44,7 @@ func (ss SearchService) GetByUserName(q string) ([]Post, error) {
 
 	stmt := db.GetDBConn()
 	stmt = stmt.Table("post")
-	stmt = stmt.Select("post.id,post.url,post.message")
+	stmt = stmt.Select("post.id,post.user_id,post.title,post.url,post.message")
 	stmt = stmt.Joins("INNER JOIN user on user.id = post.user_id")
 	stmt = stmt.Where("user.name LIKE ?", "%"+q+"%")
 	if err := stmt.Find(&p).Error; err != nil {
@@ -45,7 +59,7 @@ func (ss SearchService) GetByTagId(q string) ([]Post, error) {
 
 	stmt := db.GetDBConn()
 	stmt = stmt.Table("post")
-	stmt = stmt.Select("post.id,post.url,post.message")
+	stmt = stmt.Select("post.id,post.user_id,post.title,post.url,post.message")
 	stmt = stmt.Joins("INNER JOIN post_tag on post_tag.post_id = post.id")
 	stmt = stmt.Joins("INNER JOIN tag on post_tag.tag_id = tag.id")
 	stmt = stmt.Where("tag.id = ?", q)
@@ -61,7 +75,7 @@ func (ss SearchService) GetByTagName(q string) ([]Post, error) {
 
 	stmt := db.GetDBConn()
 	stmt = stmt.Table("post")
-	stmt = stmt.Select("post.id,post.url,post.message")
+	stmt = stmt.Select("post.id,post.user_id,post.title,post.url,post.message")
 	stmt = stmt.Joins("INNER JOIN post_tag on post_tag.post_id = post.id")
 	stmt = stmt.Joins("INNER JOIN tag on post_tag.tag_id = tag.id")
 	stmt = stmt.Where("tag.name LIKE ?", "%"+q+"%")
