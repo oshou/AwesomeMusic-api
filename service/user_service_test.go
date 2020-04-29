1c0011560852f47b660bd1dc7986d14de764ad6f
oshou <adf1985adf@gmail.com>
2020-04-29 21:36:54 +0900

    add user_service_test

--- service/user_service_test.go
+++ service/user_service_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/oshou/AwesomeMusic-api/domain/model"
 	"github.com/oshou/AwesomeMusic-api/domain/repository"
 	"github.com/oshou/AwesomeMusic-api/mock/mock_repository"
 )
@@ -24,12 +25,7 @@ func TestNewUserService(t *testing.T) {
 		want IUserService
 	}{
 		{
-			name: "aaa-test",
-			args: args{repo: mockRepo},
-			want: &userService{repo: mockRepo},
-		},
-		{
-			name: "aaa-test",
+			name: "new",
 			args: args{repo: mockRepo},
 			want: &userService{repo: mockRepo},
 		},
@@ -43,107 +39,160 @@ func TestNewUserService(t *testing.T) {
 	}
 }
 
-///// func Test_userService_GetUsers(t *testing.T) {
-///// 	type fields struct {
-///// 		repo repository.IUserRepository
-///// 	}
-///// 	mockRepo := &repository.UserRepositoryMock{}
-///// 	tests := []struct {
-///// 		name    string
-///// 		fields  fields
-///// 		want    []*model.User
-///// 		wantErr bool
-///// 	}{
-///// 		{
-///// 			"test-aaa",
-///// 			fields{repo: mockRepo},
-///// 			[]*model.User{},
-///// 			false,
-///// 		},
-///// 	}
-///// 	for _, tt := range tests {
-///// 		mockRepo.GetAllMock = func() ([]*model.User, error) {
-///// 			return tt.want, nil
-///// 		}
-///// 		tt.fields.repo, _ = mockRepo.GetAllMock()
-///// 		t.Run(tt.name, func(t *testing.T) {
-///// 			uu := &userService{
-///// 				repo: tt.fields.repo,
-///// 			}
-///// 			got, err := uu.GetUsers()
-///// 			if (err != nil) != tt.wantErr {
-///// 				t.Errorf("userService.GetUsers() error = %v, wantErr %v", err, tt.wantErr)
-///// 				return
-///// 			}
-///// 			if !reflect.DeepEqual(got, tt.want) {
-///// 				t.Errorf("userService.GetUsers() = %v, want %v", got, tt.want)
-///// 			}
-///// 		})
-///// 	}
-///// }
-/////
-///// func Test_userService_GetUserByID(t *testing.T) {
-///// 	type fields struct {
-///// 		repo repository.IUserRepository
-///// 	}
-///// 	type args struct {
-///// 		userID int
-///// 	}
-///// 	tests := []struct {
-///// 		name    string
-///// 		fields  fields
-///// 		args    args
-///// 		want    *model.User
-///// 		wantErr bool
-///// 	}{
-///// 		// TODO: Add test cases.
-///// 	}
-///// 	for _, tt := range tests {
-///// 		t.Run(tt.name, func(t *testing.T) {
-///// 			uu := &userService{
-///// 				repo: tt.fields.repo,
-///// 			}
-///// 			got, err := uu.GetUserByID(tt.args.userID)
-///// 			if (err != nil) != tt.wantErr {
-///// 				t.Errorf("userService.GetUserByID() error = %v, wantErr %v", err, tt.wantErr)
-///// 				return
-///// 			}
-///// 			if !reflect.DeepEqual(got, tt.want) {
-///// 				t.Errorf("userService.GetUserByID() = %v, want %v", got, tt.want)
-///// 			}
-///// 		})
-///// 	}
-///// }
-/////
-///// func Test_userService_AddUser(t *testing.T) {
-///// 	type fields struct {
-///// 		repo repository.IUserRepository
-///// 	}
-///// 	type args struct {
-///// 		name string
-///// 	}
-///// 	tests := []struct {
-///// 		name    string
-///// 		fields  fields
-///// 		args    args
-///// 		want    *model.User
-///// 		wantErr bool
-///// 	}{
-///// 		// TODO: Add test cases.
-///// 	}
-///// 	for _, tt := range tests {
-///// 		t.Run(tt.name, func(t *testing.T) {
-///// 			uu := &userService{
-///// 				repo: tt.fields.repo,
-///// 			}
-///// 			got, err := uu.AddUser(tt.args.name)
-///// 			if (err != nil) != tt.wantErr {
-///// 				t.Errorf("userService.AddUser() error = %v, wantErr %v", err, tt.wantErr)
-///// 				return
-///// 			}
-///// 			if !reflect.DeepEqual(got, tt.want) {
-///// 				t.Errorf("userService.AddUser() = %v, want %v", got, tt.want)
-///// 			}
-///// 		})
-///// 	}
-///// }
+func Test_userService_GetUsers(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	tests := []struct {
+		name    string
+		prepare func(*mock_repository.MockIUserRepository)
+		want    []*model.User
+		wantErr bool
+	}{
+		{
+			name: "no-data",
+			prepare: func(mockRepo *mock_repository.MockIUserRepository) {
+				mockRepo.EXPECT().GetAll().Return([]*model.User{}, nil)
+			},
+			want:    []*model.User{},
+			wantErr: false,
+		},
+		{
+			name: "3-data",
+			prepare: func(mockRepo *mock_repository.MockIUserRepository) {
+				mockRepo.EXPECT().GetAll().Return([]*model.User{
+					{ID: 1, Name: "Mike"},
+					{ID: 2, Name: "Jane"},
+					{ID: 3, Name: "John"},
+				}, nil)
+			},
+			want: []*model.User{
+				{ID: 1, Name: "Mike"},
+				{ID: 2, Name: "Jane"},
+				{ID: 3, Name: "John"},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := mock_repository.NewMockIUserRepository(ctrl)
+			tt.prepare(mockRepo)
+			uu := &userService{repo: mockRepo}
+			got, err := uu.GetUsers()
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("userService.GetUsers() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("userService.GetUsers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_userService_GetUserByID(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	type args struct {
+		userID int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		prepare func(*mock_repository.MockIUserRepository)
+		want    *model.User
+		wantErr bool
+	}{
+		{
+			name: "no-data",
+			args: args{userID: 0},
+			prepare: func(mockRepo *mock_repository.MockIUserRepository) {
+				mockRepo.EXPECT().GetByID(0).Return(&model.User{}, nil)
+			},
+			want:    &model.User{},
+			wantErr: false,
+		},
+		{
+			name: "1-data",
+			args: args{userID: 1},
+			prepare: func(mockRepo *mock_repository.MockIUserRepository) {
+				mockRepo.EXPECT().GetByID(1).Return(&model.User{ID: 1, Name: "Mike"}, nil)
+			},
+			want:    &model.User{ID: 1, Name: "Mike"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := mock_repository.NewMockIUserRepository(ctrl)
+			tt.prepare(mockRepo)
+			uu := &userService{repo: mockRepo}
+			got, err := uu.GetUserByID(tt.args.userID)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("userService.GetUserByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("userService.GetUserByID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_userService_AddUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		prepare func(*mock_repository.MockIUserRepository)
+		want    *model.User
+		wantErr bool
+	}{
+		{
+			name: "no-data",
+			args: args{name: "unknown"},
+			prepare: func(mockRepo *mock_repository.MockIUserRepository) {
+				mockRepo.EXPECT().Add("unknown").Return(&model.User{}, nil)
+			},
+			want:    &model.User{},
+			wantErr: false,
+		},
+		{
+			name: "1-data",
+			args: args{name: "Mike"},
+			prepare: func(mockRepo *mock_repository.MockIUserRepository) {
+				mockRepo.EXPECT().Add("Mike").Return(&model.User{ID: 1, Name: "Mike"}, nil)
+			},
+			want:    &model.User{ID: 1, Name: "Mike"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := mock_repository.NewMockIUserRepository(ctrl)
+			tt.prepare(mockRepo)
+			uu := &userService{repo: mockRepo}
+			got, err := uu.AddUser(tt.args.name)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("userService.AddUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("userService.AddUser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
