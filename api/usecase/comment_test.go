0f2384548f76a3b448d812d3172e00f598bd3fe5
oshou <adf1985adf@gmail.com>
2021-05-16 02:22:39 +0900

    fix usecase test

--- api/usecase/comment_test.go
+++ api/usecase/comment_test.go
@@ -3,12 +3,9 @@ package usecase
 import (
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-
 	"github.com/oshou/AwesomeMusic-api/api/domain/model"
 	"github.com/oshou/AwesomeMusic-api/api/domain/repository"
-	"github.com/oshou/AwesomeMusic-api/api/mock/mock_repository"
 )
 
 func TestNewCommentUsecase(t *testing.T) {
@@ -32,8 +29,6 @@ func TestNewCommentUsecase(t *testing.T) {
 }
 
 func Test_commentUsecase_ListComments(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
 	type args struct {
 		postID int
 	}
@@ -44,28 +39,11 @@ func Test_commentUsecase_ListComments(t *testing.T) {
 		want    []*model.Comment
 		wantErr bool
 	}{
-		{
-			name: "Success",
-			args: args{postID: 1},
-			want: []*model.Comment{
-				{
-					ID:      1,
-					UserID:  1,
-					PostID:  1,
-					Comment: "aaa",
-				},
-			},
-			wantErr: false,
-		},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mock := mock_repository.NewMockICommentRepository(ctrl)
-			mock.EXPECT().List(tt.args.postID).Return(tt.want, tt.wantErr)
-
-			cu := NewCommentUsecase(mock)
-			got, err := cu.ListComments(tt.args.postID)
+			got, err := tt.cu.ListComments(tt.args.postID)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("commentUsecase.ListComments() error = %v, wantErr %v", err, tt.wantErr)
 				return
